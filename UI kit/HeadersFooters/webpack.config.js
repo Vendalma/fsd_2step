715537a6275e29cb40f0bd0f34548d8d3d3e701c
Olga Maurer <olga.maurer898@gmail.com>
2020-06-19 20:45:35 +0300

    header&footers

---
+++ UI kit/HeadersFooters/webpack.config.js
@@ -0,0 +1,58 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const loader = require("sass-loader");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  devServer: {
+    contentBase: "./dist",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          MiniCssExtractPlugin.loader,
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.pug$/,
+        use: ["pug-loader"],
+      },
+      {
+        test: /\.(woff|ttf|svg)$/,
+        use: [
+          {
+            loader: "file-loader",
+          },
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ["file-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      template: "./src/index.pug",
+      inject: false,
+    }),
+    new MiniCssExtractPlugin({
+      filename: "style.css",
+    }),
+  ],
+};
