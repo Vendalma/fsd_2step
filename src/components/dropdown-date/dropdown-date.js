58bbf44160579dd27d42e0277df02f170c761a29
Olga Maurer <olga.maurer898@gmail.com>
2020-07-01 12:45:06 +0300

    Добавила кнопки для календаря, исправила стили text field

--- src/components/dropdown-date/dropdown-date.js
+++ src/components/dropdown-date/dropdown-date.js
@@ -1,9 +1,35 @@
 import "air-datepicker/dist/js/datepicker";
+//import "../../library/air-datapicker/air-datapicker";
 import "./dropdown-date.scss";
 
-/*const pic = require("air-datepicker");
-let newpic = new pic();
-let drop = document.querySelector(".dropdown-date__input");
-*/
 import "air-datepicker";
-$(".dropdown-date__input").datepicker({});
+$(".dropdown-date__input").datepicker({
+  navTitles: {
+    days: "MM yyyy",
+  },
+  prevHtml: '<i class="material--icon">arrow_back</i>',
+  nextHtml: '<i class="material--icon">arrow_forward</i>',
+  clearButton: true,
+});
+let input_filter = document.querySelector(".dropdown-date__input_filter");
+input_filter.setAttribute("data-multiple-dates-separator", " - ");
+$(".dropdown-date__input_filter").datepicker({
+  navTitles: {
+    days: "MM yyyy",
+  },
+  prevHtml: '<i class="material--icon">arrow_back</i>',
+  nextHtml: '<i class="material--icon">arrow_forward</i>',
+  clearButton: true,
+  range: true,
+});
+
+let dataPicker = document.querySelectorAll(".datepicker--buttons");
+
+for (let elem of dataPicker) {
+  let btn = document.createElement("span");
+  btn.classList.add("datepicker--button");
+  btn.classList.add("datepicker--button-color_one");
+  btn.setAttribute("data-action", "button");
+  btn.innerHTML = "Применить";
+  elem.appendChild(btn);
+}
