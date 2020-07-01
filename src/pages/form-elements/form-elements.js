7850b005c3ceb6efb6bceddd853424d07b80e421
Olga Maurer <olga.maurer898@gmail.com>
2020-07-01 17:12:28 +0300

    Checkbox buttons, radio buttons

--- src/pages/form-elements/form-elements.js
+++ src/pages/form-elements/form-elements.js
@@ -3,4 +3,7 @@ import "@layout/uikit/uikit.js";
 import "@components/field/field";
 import "@components/dropdown/dropdown";
 import "@components/dropdown-date/dropdown-date";
+import "@components/checkbox/checkbox";
+import "@components/radio/radio";
+
 import "@/main.scss";
