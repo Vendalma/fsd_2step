7b753267fc6921ecf2831d410dd50427080f7083
Olga Maurer <olga.maurer898@gmail.com>
2020-07-16 15:34:00 +0300

    Добавила кнопку применить для календаря через функцию, начала верстать форму регистрации

--- src/components/dropdown-date/dropdown-date.js
+++ src/components/dropdown-date/dropdown-date.js
@@ -3,35 +3,6 @@ import "./dropdown-date.scss";
 import "../description/description";
 import "air-datepicker";
 
-$(function () {
-  let $start = $("#start"),
-    $end = $("#end");
-
-  let picker = $("#start")
-    .datepicker({
-      range: true,
-      multipleDatesSeparator: "-",
-      language: "en",
-      navTitles: {
-        days: "MM yyyy",
-      },
-      keyboardNav: false,
-      clearButton: true,
-      prevHtml: '<i class="material--icon">arrow_back</i>',
-      nextHtml: '<i class="material--icon">arrow_forward</i>',
-
-      onSelect: function (fd, d, picker) {
-        $("#start").val(fd.split("-")[0]);
-        $("#end").val(fd.split("-")[1]);
-      },
-    })
-    .data("datepicker");
-
-  $("#end").on("click", () => {
-    picker.show();
-  });
-});
-
 $(".dropdown-date__input_filter").datepicker({
   navTitles: {
     days: "MM yyyy",
@@ -59,15 +30,60 @@ $(".dropdown-date__input_filter").datepicker({
   keyboardNav: false,
 });
 
-let datePicker = document.querySelectorAll(".datepicker");
+let datePicker = document.querySelectorAll(".datepicker-here");
 
 datePicker.forEach((elem) => {
-  console.log(1);
-  let buttons = elem.querySelector(".datepicker--buttons");
-  let btn = document.createElement("span");
-  btn.classList.add("datepicker--button");
-  btn.classList.add("datepicker--button-color_one");
-  btn.setAttribute("data-action", "button");
-  btn.innerHTML = "Применить";
-  buttons.appendChild(btn);
+  $(elem).datepicker({
+    range: true,
+    multipleDates: 2,
+    multipleDatesSeparator: "-",
+    language: "ru",
+    navTitles: {
+      days: "MM yyyy",
+    },
+    keyboardNav: false,
+    clearButton: true,
+    prevHtml: '<i class="material--icon">arrow_back</i>',
+    nextHtml: '<i class="material--icon">arrow_forward</i>',
+  });
+
+  if (elem.classList.contains("dropdown-date__inline")) {
+    inline(elem);
+  } else if (elem.classList.contains("dropdown-date__input_double")) {
+    double(elem);
+  }
+
+  function inline(elem) {
+    addButtons($(elem));
+  }
+
+  function double(elem) {
+    let start = elem;
+    let end = document.querySelector(".dropdown-date__input-end");
+
+    $(start).datepicker({
+      onSelect: function (fd, d, picker) {
+        $(start).val(fd.split("-")[0]);
+        $(end).val(fd.split("-")[1]);
+      },
+    });
+    $(end).on("click", () => {
+      $(start).data("datepicker").show();
+    });
+    addButtons($(start));
+  }
+
+  function addButtons(elem) {
+    let applyButton = document.createElement("span");
+    applyButton.setAttribute("data-action", "apply");
+    applyButton.classList.add("datepicker--button");
+    applyButton.classList.add("datepicker--button-colored");
+    applyButton.innerHTML = "Применить";
+
+    let buttons = elem
+      .data("datepicker")
+      .$datepicker.find(".datepicker--buttons");
+
+    buttons.append(applyButton);
+  }
 });
--- src/pages/cards/cards.js
+++ src/pages/cards/cards.js
@@ -2,5 +2,7 @@ import "./cards.scss";
 
 import "@layout/uikit/uikit";
 import "@layout/cards/search-card/search-card";
+import "@components/dropdown-date/dropdown-date";
+import "@layout/cards/registration-card/registration-carg";
 
 import "@/main.scss";
