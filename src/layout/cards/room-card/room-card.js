870311d060ae5bcaf07646af3c1f888c0a88943b
Olga Maurer <olga.maurer898@gmail.com>
2020-07-20 16:32:34 +0300

    Полностью сделана карточка комнат, подключен слайдер картинок, исправлены стили календаря

---
+++ src/layout/cards/room-card/room-card.js
@@ -0,0 +1,17 @@
+import "./room-card.scss";
+
+import "@components/rate button/rate button";
+import "lightslider/dist/js/lightslider";
+import "lightslider";
+
+let roomslider = document.querySelectorAll(".room-card__slider");
+
+roomslider.forEach((elem) => {
+  $(document).ready(function () {
+    $(elem).lightSlider({
+      item: 1,
+      nextHtml: '<i class="material-icons">expand_more</i>',
+      prevHtml: '<i class="material-icons">expand_more</i>',
+    });
+  });
+});
--- src/pages/cards/cards.js
+++ src/pages/cards/cards.js
@@ -5,5 +5,6 @@ import "@layout/cards/search-card/search-card";
 import "@components/dropdown-date/dropdown-date";
 import "@layout/cards/registration-card/registration-carg";
 import "@layout/cards/sign-in-card/sign-in-card";
+import "@layout/cards/room-card/room-card";
 
 import "@/main.scss";
