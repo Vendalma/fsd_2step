5f6705faa3d25a99bc8a9baac45d828dbbe5e8bd
Olga Maurer <olga.maurer898@gmail.com>
2020-07-29 11:08:39 +0300

    Landing page сверстана, сделаны правки inputmask

--- src/components/field/field.js
+++ src/components/field/field.js
@@ -1,7 +1,8 @@
 import "./field.scss";
 
-let mask_field = document.querySelector(".text-field__input_mask");
+let $mask = $(".text-field").find(".text-field__input_mask");
+//let mask_field = document.querySelector(".text-field__input_mask");
 var Inputmask = require("inputmask");
 
 var im = new Inputmask("99.99.9999");
-im.mask(mask_field);
+im.mask($mask);
--- src/pages/landing/landing.js
+++ src/pages/landing/landing.js
@@ -4,5 +4,36 @@ import "@layout/website-base/website-base";
 import "@layout/header/header";
 import "@layout/footer/footer";
 import "@layout/footer-small/footer-small";
+import "@layout/cards/search-card/search-card";
 
 import "@/main.scss";
+
+//идея реализации слайдера позаимствоана здесь --> http://gnatkovsky.com.ua/plavnaya-smena-fona-s-ispolzovaniem-jquery.html
+
+$(function () {
+  var transTime = 5000;
+  var numBgColors = $(".landing-page__slider_item").length;
+  var bgtrans = setInterval(function () {
+    if (
+      $(".landing-page__slider_active").index() ==
+      $(".landing-page__slider_item").length - 1
+    ) {
+      $(".landing-page__slider_active").removeClass(
+        "landing-page__slider_active"
+      );
+      $(".landing-page__slider_item")
+        .eq(0)
+        .addClass("landing-page__slider_active");
+    } else {
+      var curIndex = $(
+        ".landing-page__slider_item.landing-page__slider_active"
+      ).index();
+      $(".landing-page__slider_item.landing-page__slider_active").removeClass(
+        "landing-page__slider_active"
+      );
+      $(".landing-page__slider_item")
+        .eq(curIndex + 1)
+        .addClass("landing-page__slider_active");
+    }
+  }, transTime);
+});
