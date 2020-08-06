a4faa94f0a21176b4ba01f9ac5c207288a0ef562
Olga Maurer <olga.maurer898@gmail.com>
2020-08-06 15:40:35 +0300

    Изменены иконки menu burger в header

--- src/layout/header/header.js
+++ src/layout/header/header.js
@@ -7,8 +7,12 @@ let buttonBurger = document.querySelectorAll(".header__burger-menu");
 
 buttonBurger.forEach((elem) => {
   let blockMenu = elem.nextElementSibling;
+  let iconMenu = elem.querySelector(".header__block_menu");
+  let iconClose = elem.querySelector(".header__block_close");
 
   elem.addEventListener("click", function () {
     blockMenu.classList.toggle("header__menu_open");
+    iconMenu.classList.toggle("header__block_active");
+    iconClose.classList.toggle("header__block_active");
   });
 });
