e4b3fba4a638ca84b79a8a92667ee45e9f252319
Vendalma <66669472+Vendalma@users.noreply.github.com>
2020-12-01 17:39:13 +0300

    Рефакторинг страницы colors-types

--- src/pages/colors-types/colors-types.js
+++ src/pages/colors-types/colors-types.js
@@ -1,6 +1,6 @@
 import "@/main.scss";
 import "@components/colors/colors.js";
-import "@components/sample-text/sample-text.js";
+import "@components/text-types/text-types.js";
 import "@layout/ui-kit/ui-kit.js";
 import "./colors-types.scss";
 
