2eee64dee3c25c556b51acb1677bbbf22b12437f
Vendalma <66669472+Vendalma@users.noreply.github.com>
2020-12-17 11:31:03 +0300

    Рефакторинг страницы для регистрации

--- src/pages/registration/registration.js
+++ src/pages/registration/registration.js
@@ -5,34 +5,14 @@ import "@components/header/header";
 import "@components/registration-card/registration-card";
 import "./registration.scss";
 
-
-
-//идея реализации слайдера позаимствована здесь --> http://gnatkovsky.com.ua/plavnaya-smena-fona-s-ispolzovaniem-jquery.html
-
-$(function () {
-  var transTime = 40000;
-  var numBgColors = $(".registration-page__slider_item").length;
-  var bgtrans = setInterval(function () {
-    if (
-      $(".registration-page__slider_active").index() ==
-      $(".registration-page__slider_item").length - 1
-    ) {
-      $(".registration-page__slider_active").removeClass(
-        "registration-page__slider_active"
-      );
-      $(".registration-page__slider_item")
-        .eq(0)
-        .addClass("registration-page__slider_active");
-    } else {
-      var curIndex = $(
-        ".registration-page__slider_item.registration-page__slider_active"
-      ).index();
-      $(
-        ".registration-page__slider_item.registration-page__slider_active"
-      ).removeClass("registration-page__slider_active");
-      $(".registration-page__slider_item")
-        .eq(curIndex + 1)
-        .addClass("registration-page__slider_active");
-    }
-  }, transTime);
-});
+//change background by --> https://www.youtube.com/watch?v=NTqmimXu1XM
+let registrationContent = document.querySelector('.js-registration__content');
+let images = [
+  'url("/assets/images/bg-image-1.jpg")',
+  'url("/assets/images/bg-image-4.jpg")',
+]
+function changeBg() {
+  let bg = images[Math.floor(Math.random() * images.length)]
+  registrationContent.style.backgroundImage = bg;
+}
+setInterval(changeBg, 30000)
