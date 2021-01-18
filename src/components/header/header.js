f90418210c6b7f09071c384855d7fd41c62379e3
Vendalma <66669472+Vendalma@users.noreply.github.com>
2021-01-18 15:02:57 +0300

    Рефакторинг компонентов страниц form-elements, headers-footers, color-types, cards

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -6,14 +6,14 @@ import "./header.scss";
 let buttonBurger = document.querySelectorAll(".js-header__burger-menu");
 
 buttonBurger.forEach((elem) => {
-  let blockMenu = elem.parentElement.querySelector('.js-header__menu');
+  let navBar = elem.parentElement.querySelector('.js-header__nav-bar');
   let iconMenu = elem.querySelector(".js-header__burger-menu_open");
   let iconClose = elem.querySelector(".js-header__burger-menu_close");
 
   elem.addEventListener("click", bindThis.bind(this));
 
   function bindThis() {
-    blockMenu.classList.toggle("header__menu_open");
+    navBar.classList.toggle("header__nav-bar_open");
     iconMenu.classList.toggle("header__burger-menu_hidden");
     iconClose.classList.toggle("header__burger-menu_hidden");
   }
--- src/components/like-button/like-button.js
+++ src/components/like-button/like-button.js
@@ -1,1 +1,34 @@
-import "./like-button.scss";
+import './like-button.scss';
+class Like {
+  constructor(container) {
+    this.container = container;
+    this.init();
+    this.changeCount();
+  }
+
+  init() {
+    this.count = this.container.querySelector('.js-like-button__count');
+    this.like = this.container.querySelector('.js-like-button__like');
+  }
+
+  changeCount() {
+    this.container.addEventListener('click', this.onContainerClick.bind(this));
+  }
+
+  onContainerClick() {
+    if (this.container.classList.contains('like-button_active')) {
+      this.container.classList.remove('like-button_active');
+      this.like.classList.remove('like-button__like_active');
+      this.count.innerHTML = Number(this.count.innerHTML) - 1;
+    } else {
+      this.container.classList.add('like-button_active');
+      this.like.classList.add('like-button__like_active');
+      this.count.innerHTML = Number(this.count.innerHTML) + 1;
+    }
+  }
+}
+
+const element = document.querySelectorAll('.js-like-button');
+element.forEach((el) => {
+  let likeCounter = new Like(el);
+});
