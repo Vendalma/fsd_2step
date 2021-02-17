b36dd252e7ef375e0cbd2fcd0b1a7ed285b0ffc8
Olga Maurer <olga.maurer898@gmail.com>
2021-02-17 14:33:06 +0300

    #10 Рефакторинг компонента donut-chart
    - ф-я отрисовки лейбла диаграммы вынесена в отдельный метод
    - исправлены ошибки линтера

--- src/components/donut-chart/donut-chart.js
+++ src/components/donut-chart/donut-chart.js
@@ -27,57 +27,55 @@ class DonutChart {
 
   createText() {
     const chartText = this.container.parentElement.querySelector(
-      '.js-chart__text'
+      '.js-chart__text',
     );
     const chartTextCount = this.container.parentElement.querySelector(
-      '.js-chart__count'
+      '.js-chart__count',
     );
-    chartText.innerHTML = `${this.setRightName(
-      this.totalCount,
-      this.text.split('|')
-    )}`;
+    chartText.innerHTML = `${this.setRightName()}`;
     chartTextCount.innerHTML = `${this.totalCount}`;
     this.setTextPosition();
   }
 
   setTextPosition() {
     const textContainer = this.container.parentElement.querySelector(
-      '.js-chart__info'
+      '.js-chart__info',
     );
     textContainer.style.left = `${
       (this.container.offsetWidth - textContainer.offsetWidth) / 2
     }px`;
   }
 
-  setRightName(num, arrItemName) {
-    num = Math.abs(num) % 100;
-    const num1 = num % 10;
-    if (num > 10 && num < 20) {
-      return arrItemName[2];
+  setRightName() {
+    const calcTotal = Math.abs(this.totalCount) % 100;
+    const calcRemainder = calcTotal % 10;
+    const itemNames = this.text.split('|');
+    if (calcTotal > 10 && calcTotal < 20) {
+      return itemNames[2];
     }
-    if (num1 > 1 && num1 < 5) {
-      return arrItemName[1];
+    if (calcRemainder > 1 && calcRemainder < 5) {
+      return itemNames[1];
     }
-    if (num1 == 1) {
-      return arrItemName[0];
+    if (calcRemainder === 1) {
+      return itemNames[0];
     }
-    return arrItemName[2];
+    return itemNames[2];
   }
+
   createColors() {
     const chartContainer = this.container.getContext('2d');
     const resultColors = [];
     this.backgroundColor.forEach((color) => {
-      let { name, colors } = color;
-      name = chartContainer.createLinearGradient(0, 0, 0, 600);
-      name.addColorStop(0, colors[0]);
-      name.addColorStop(1, colors[1]);
-      resultColors.push(name);
+      const chartColor = chartContainer.createLinearGradient(0, 0, 0, 600);
+      chartColor.addColorStop(0, color[0]);
+      chartColor.addColorStop(1, color[1]);
+      resultColors.push(chartColor);
     });
     return resultColors;
   }
 
   initDiagram() {
-    new Chart(this.container, {
+    const chart = new Chart(this.container, {
       type: 'doughnut',
       data: {
         labels: this.getOptions('text'),
@@ -98,57 +96,48 @@ class DonutChart {
         maintainAspectRatio: false,
         tooltips: {
           enabled: false,
-
-          custom: function ({ opacity, yAlign, body, caretX, caretY }) {
-            let tooltipEl = document.getElementById('chartjs-tooltip');
-            if (!tooltipEl) {
-              tooltipEl = document.createElement('div');
-              tooltipEl.id = 'chartjs-tooltip';
-              tooltipEl.innerHTML = '<table></table>';
-              document.body.appendChild(tooltipEl);
-              tooltipEl.classList.add('chart__diagram-label');
-            }
-
-            if (opacity === 0) {
-              tooltipEl.classList.add('chart__diagram-label_hidden');
-            } else {
-              tooltipEl.classList.remove('chart__diagram-label_hidden');
-            }
-
-            tooltipEl.classList.remove('above', 'below', 'no-transform');
-            if (yAlign) {
-              tooltipEl.classList.add(yAlign);
-            } else {
-              tooltipEl.classList.add('no-transform');
-            }
-
-            function getBody(bodyItem) {
-              return bodyItem.lines;
-            }
-
-            if (body) {
-              const bodyLines = body.map(getBody);
-              let innerHtml = '<tbody>';
-              bodyLines.forEach(function (body) {
-                innerHtml += `<tr><td>${body}</td></tr>`;
-              });
-              innerHtml += '</tbody>';
-
-              const tableRoot = tooltipEl.querySelector('table');
-              tableRoot.innerHTML = innerHtml;
-            }
-            const position = this._chart.canvas.getBoundingClientRect();
-            tooltipEl.classList.add('chart__diagram-label');
-            tooltipEl.style.left = `${
-              position.left + window.pageXOffset + caretX
-            }px`;
-            tooltipEl.style.top = `${
-              position.top + window.pageYOffset + caretY
-            }px`;
-          },
+          custom: this.createCustomLabel,
         },
       },
     });
+  }
+
+  createCustomLabel({
+    opacity, yAlign, body, caretX, caretY,
+  }) {
+    let tooltipEl = document.getElementById('chartjs-tooltip');
+    if (!tooltipEl) {
+      tooltipEl = document.createElement('div');
+      tooltipEl.id = 'chartjs-tooltip';
+      tooltipEl.innerHTML = '<table></table>';
+      document.body.appendChild(tooltipEl);
+      tooltipEl.classList.add('chart__diagram-label');
+    }
+
+    if (opacity === 0) {
+      tooltipEl.classList.add('chart__diagram-label_hidden');
+    } else {
+      tooltipEl.classList.remove('chart__diagram-label_hidden');
+    }
+
+    tooltipEl.classList.remove('above', 'below', 'no-transform');
+    if (yAlign) {
+      tooltipEl.classList.add(yAlign);
+    } else {
+      tooltipEl.classList.add('no-transform');
+    }
+
+    if (body) {
+      const tableRoot = tooltipEl.querySelector('table');
+      body.forEach((bodyElem) => {
+        tableRoot.innerHTML = `<tbody><tr><td>${bodyElem.lines[0]}</td></tr></tbody>`;
+      });
+    }
+    /* eslint-disable-next-line */
+    const position = this._chart.canvas.getBoundingClientRect();
+    tooltipEl.classList.add('chart__diagram-label');
+    tooltipEl.style.left = `${position.left + window.pageXOffset + caretX}px`;
+    tooltipEl.style.top = `${position.top + window.pageYOffset + caretY}px`;
   }
 }
 export default DonutChart;
--- src/components/donut-chart/init-donut-chart.js
+++ src/components/donut-chart/init-donut-chart.js
@@ -1,4 +1,5 @@
 import DonutChart from './donut-chart';
+
 const { chartOptions, backgroundColors } = require('./donut-chart.json');
 
 const myChart = document.querySelector('.js-chart__diagram');
