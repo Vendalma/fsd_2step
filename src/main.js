54e6158154f8c876b33009feef4d359372601e5c
Olga Maurer <olga.maurer898@gmail.com>
2021-03-04 16:24:48 +0300

    #4 Изменены глобальные стили,  удален normalize.css

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
-import 'normalize.css';
+import '@fortawesome/fontawesome-free';
+import '../node_modules/@fortawesome/fontawesome-free/css/brands.min.css';
+import '../node_modules/@fortawesome/fontawesome-free/css/fontawesome.min.css';
 import './assets/fonts/fonts.scss';
-import './assets/scss/base-settings/base-settings.scss';
+import './assets/scss/base-settings.scss';
