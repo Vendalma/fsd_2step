f17bf1ed544180b1e5ae97c9528fa285e32cf9e3
Olga Maurer <olga.maurer898@gmail.com>
2021-03-09 09:17:52 +0300

    #4 Изменены пути

--- src/components/room-card/room-card.js
+++ src/components/room-card/room-card.js
@@ -1,4 +1,4 @@
 import '@components/rate-button/rate-button';
 import 'lightslider';
-import '../../../node_modules/lightslider/dist/css/lightslider.min.css';
+import 'lightslider/dist/css/lightslider.min.css';
 import './room-card.scss';
--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import '@fortawesome/fontawesome-free';
-import '../node_modules/@fortawesome/fontawesome-free/css/brands.min.css';
-import '../node_modules/@fortawesome/fontawesome-free/css/fontawesome.min.css';
+import '@fortawesome/fontawesome-free/css/brands.min.css';
+import '@fortawesome/fontawesome-free/css/fontawesome.min.css';
 import './assets/fonts/fonts.scss';
 import './assets/scss/base-settings.scss';
